4eb1058a7292621e9172451b166722e6ef842e1f
popuga <pinchuk.andryusha310212@gmail.com>
2023-02-28 23:52:48 +0200

    refactor: minor improvements

--- src/components/products/Product.tsx
+++ src/components/products/Product.tsx
@@ -1,6 +1,5 @@
 import type ProductType from "../../types/Product";
 import { addCartItem } from "../../stores/cartStore";
-import { useState } from "preact/hooks";
 
 interface ProductProps {
   product: ProductType;
@@ -20,18 +19,6 @@ const Product = ({ product }: ProductProps) => {
               width="auto"
               loading="lazy"
             />
-            {/* {isImageLoading && (
-              <div class="flex items-center justify-center">
-                <div
-                  class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                  role="status"
-                >
-                  <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                    Loading...
-                  </span>
-                </div>
-              </div>
-            )} */}
           </div>
         </div>
         <div
