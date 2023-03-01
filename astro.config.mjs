f35fcef52e6b8d97f6a187eca2da54754f660f12
Andrey <pinchuk.andryusha310212@gmail.com>
2023-03-01 12:58:49 +0200

    chore: add sitemap and compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), image()],
+  integrations: [
+    tailwind(),
+    preact(),
+    image(),
+    compress(),
+    sitemap({
+      // filter **/admin/** pages
+      filter: (page) => page.match(/\/admin\//),
+    }),
+  ],
 });
