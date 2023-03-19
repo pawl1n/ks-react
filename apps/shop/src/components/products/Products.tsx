dd2fc573931982f25952e8a89a97ce3664e4a26b
Andrey <pinchuk.andryusha310212@gmail.com>
2023-03-19 16:13:02 +0200

    feature: add infinite scroll

--- apps/shop/src/components/products/Products.tsx
+++ apps/shop/src/components/products/Products.tsx
@@ -1,23 +1,50 @@
-import { memo } from "preact/compat";
 import Product from "./Product";
 import type ProductType from "../../types/Product";
+import { useEffect, useState } from "preact/hooks";
+import { getAll } from "../../api/Get";
 
 let isLoading = false;
 
-const products: ProductType[] = await fetch(
-  "http://localhost:8080/api/products"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    isLoading = false;
+const Products = () => {
+  const [products, setProducts] = useState([] as ProductType[]);
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
 
-    return data?._embedded?.products ?? [];
-  })
-  .catch(() => {
-    isLoading = false;
+  const params = new URLSearchParams({
+    size: "5",
+    page: String(page),
   });
 
-const Products = memo(() => {
+  useEffect(() => {
+    getAll<ProductType>("products", params).then((data) => {
+      if (!data?._embedded?.products) {
+        setIsLastPage(true);
+        return;
+      }
+      setProducts([...products, ...data._embedded.products]);
+    });
+  }, [page]);
+
+  useEffect(() => {
+    // setup infinite scroll listeners
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (isLastPage) return;
+    const scrollTop = document.documentElement.scrollTop;
+    const offsetHeight = document.documentElement.offsetHeight;
+    const clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight >= offsetHeight) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
   return (
     <section className="py-16">
       <div className="container mx-auto">
@@ -33,6 +60,6 @@ const Products = memo(() => {
       </div>
     </section>
   );
-});
+};
 
 export default Products;
