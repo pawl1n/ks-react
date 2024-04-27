769d3e4826320002963d2fca4c3086d5a72f93d9
Pinchuk Andrii <pinchuk.andryusha310212@gmail.com>
2024-04-27 21:06:36 +0300

    chore(deps): upgrade deps

--- apps/shop/astro.config.mjs
+++ apps/shop/astro.config.mjs
@@ -1,17 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-import compress from "astro-compress";
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [tailwind(), preact(), image(), sitemap(), compress()]
+  integrations: [tailwind(), preact(), sitemap()],
 });
